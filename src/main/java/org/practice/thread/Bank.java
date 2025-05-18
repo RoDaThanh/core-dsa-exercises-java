f64da3c3fac383c5d1cca84c63894dcaac2d06da
thanh.ro <thanhroda1@gmail.com>
2025-05-18 11:07:12 +0700

    refactor: use synchroized lock

--- src/main/java/org/practice/thread/Bank.java
+++ src/main/java/org/practice/thread/Bank.java
@@ -6,28 +6,29 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Bank {
 
-    private double[] accounts;
+    private final double[] accounts;
 
-    private Lock bankLock = new ReentrantLock();
 
     public Bank(int numberOfAccount, double initialBalance) {
         accounts = new double[numberOfAccount];
         Arrays.fill(accounts, initialBalance);
     }
 
-    public void transfer(int from, int to, double amount) {
-        bankLock.lock();
-        try {
-            if (this.accounts[from] < amount) return;
-            this.accounts[from] -= amount;
-            this.accounts[to] += amount;
-            System.out.printf(Thread.currentThread()+ "%10.2f from %d to %d", amount, from, to);
-            System.out.println();
-            System.out.printf("Total balance: %10.2f", getTotalBalance());
-            System.out.println();
-        } finally {
-            bankLock.unlock();
+    public synchronized void transfer(int from, int to, double amount) throws InterruptedException {
+        while (accounts[from] < amount) {
+            wait();
         }
+
+        this.accounts[from] -= amount;
+        this.accounts[to] += amount;
+
+        notifyAll();
+
+        System.out.printf(Thread.currentThread()+ "%10.2f from %d to %d", amount, from, to);
+        System.out.println();
+        System.out.printf("Total balance: %10.2f", getTotalBalance());
+        System.out.println();
+
     }
 
     private double getTotalBalance() {
