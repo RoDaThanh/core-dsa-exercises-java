949d683adef8cea7fdf56e756c32cff356eca3e1
thanh.ro <thanhroda1@gmail.com>
2025-05-21 20:36:17 +0700

    fix: add wait() again

--- src/main/java/org/practice/thread/Bank.java
+++ src/main/java/org/practice/thread/Bank.java
@@ -14,16 +14,15 @@ public class Bank {
 
     public synchronized void transfer(int from, int to, double amount) throws InterruptedException {
         while (accounts[from] < amount) {
+            wait();
         }
 
         this.accounts[from] -= amount;
         this.accounts[to] += amount;
 
-        notifyAll();
-
         System.out.printf(Thread.currentThread()+ "%10.2f from %d to %d%n", amount, from, to);
         System.out.printf("Total balance: %10.2f%n", getTotalBalance());
-
+        notifyAll();
     }
 
     private double getTotalBalance() {
