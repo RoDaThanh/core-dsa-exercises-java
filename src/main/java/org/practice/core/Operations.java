9bde998447bae5fca338d46478a1728b7d34957e
thanh.ro <thanhroda1@gmail.com>
2025-06-30 20:39:52 +0700

    ex: operations

---
+++ src/main/java/org/practice/core/Operations.java
@@ -0,0 +1,20 @@
+package org.practice.core;
+
+public class Operations {
+    public static void main(String[] args) {
+//        int i = 8;
+//        int k = i++;
+//        System.out.println(k);
+//        System.out.println(i);
+
+        for (int i = 0; i < 10; i++) {
+//            if (i == 5) {
+//                continue;
+//            }
+            if (i == 6) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
